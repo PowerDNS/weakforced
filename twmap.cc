2ecb9ca683b2cf0bc0d9608d7725d2808084f0ac
Neil Cook <neil.cook@noware.co.uk>
2015-10-22 12:45:30 -0400

    Finished base Time Window framework. Added support for hyperloglog type

--- twmap.cc
+++ twmap.cc
@@ -1,5 +1,6 @@
 #include "twmap.hh"
 #include <iostream>
+#include <unistd.h>
 
 static TWStatsTypeMap g_field_types;
 
@@ -16,8 +17,35 @@ bool TWStatsDB<T>::setFields(std::shared_ptr<FieldMap> fieldsp)
 }
 
 template <typename T>
-bool TWStatsDB<T>::find_create_key_field(const T& key, const std::string& field_name, std::vector<TWStatsMemberP>& ret_vec)
+void TWStatsDB<T>::clean_windows(TWStatsBuf& twsb)
 {
+  // this function checks the time difference since each array member was first written
+  // and expires (zeros) any windows as necessary. This needs to be done before any data is read or written
+  std::time_t now, expire_diff;
+  std::time(&now);
+  expire_diff = window_size * num_windows;
+
+  for (TWStatsBuf::iterator i = twsb.begin(); i != twsb.end(); ++i) {
+    std::time_t last_write = i->first;
+    auto sm = i->second;
+    if ((last_write != 0) && (now - last_write) >= expire_diff) {
+      sm->erase();
+      i->first = 0;
+    }
+  }
+}
+
+template <typename T>
+bool TWStatsDB<T>::find_key_field(const T& key, const std::string& field_name, TWStatsBuf*& ret_vec)
+{
+  return find_create_key_field(key, field_name, ret_vec, false);
+}
+
+template <typename T>
+bool TWStatsDB<T>::find_create_key_field(const T& key, const std::string& field_name, TWStatsBuf*& ret_vec, 
+					 bool create)
+{
+  TWStatsBuf myrv;
   // first check if the field name is in the field map - if not we throw the query out straight away
   auto myfield = field_map->find(field_name);
   if (myfield == field_map->end())
@@ -35,59 +63,203 @@ bool TWStatsDB<T>::find_create_key_field(const T& key, const std::string& field_
     auto myfm = mysdb->second.find(field_name);
     if (myfm != mysdb->second.end()) {
       // awesome this key/field combination has already been created
-      auto mystats = myfm->second;
-      ret_vec = mystats;
-      return true;
+      ret_vec = &(myfm->second);
+      // whenever we retrieve a set of windows, we clean them to remove expired windows
+      clean_windows(myfm->second);
+      cur_window = current_window();
+      return(true);
     }
     else {
-      // if we get here it means the key exists, but not the field so we need to add it
-      auto mystat = mytype->second;
-      for (int i=0; i< num_windows; i++) {
-	ret_vec.push_back(mystat());
+      if (create) {
+	// if we get here it means the key exists, but not the field so we need to add it
+	auto mystat = mytype->second;
+	for (int i=0; i< num_windows; i++) {
+	  myrv.push_back(std::pair<std::time_t, TWStatsMemberP>((std::time_t)0, mystat()));
+	}
+	mysdb->second.insert(std::pair<std::string, TWStatsBuf>(field_name, myrv));
       }
-      mysdb->second.insert(std::pair<std::string, std::vector<TWStatsMemberP>>(field_name, ret_vec));
     }
   }
   else {
-    // if we get here, we have no key and no field
-    auto mystat = mytype->second;
-    for (int i=0; i< num_windows; i++) {
-      ret_vec.push_back(mystat());
+    if (create) {
+      // if we get here, we have no key and no field
+      auto mystat = mytype->second;
+      for (int i=0; i< num_windows; i++) {
+	myrv.push_back(std::pair<std::time_t, TWStatsMemberP>((std::time_t)0, mystat()));
+      }
+      std::map<std::string, TWStatsBuf> myfm;
+      myfm.insert(std::pair<std::string, TWStatsBuf>(field_name, myrv));
+      stats_db.insert(std::pair<T, std::map<std::string, TWStatsBuf>>(key, myfm));
     }
-    std::map<std::string, std::vector<TWStatsMemberP>> myfm;
-    myfm.insert(std::pair<std::string, std::vector<TWStatsMemberP>>(field_name, ret_vec));
-    stats_db.insert(std::pair<T, std::map<std::string, std::vector<TWStatsMemberP>>>(key, myfm));
   }
-  return true;
+  // update the current window
+  cur_window = current_window();
+  // we created the field, now just look it up again so we get the actual pointer not a copy
+  return (find_create_key_field(key, field_name, ret_vec, false));
 }
 
 template <typename T>
 int TWStatsDB<T>::incr(const T& key, const std::string& field_name)
 {
-  std::vector<TWStatsMemberP> myvec;
+  return add(key, field_name, 1);
+}
+
+template <typename T>
+int TWStatsDB<T>::decr(const T& key, const std::string& field_name)
+{
+  return sub(key, field_name, 1);
+}
+
+template <typename T>
+int TWStatsDB<T>::add(const T& key, const std::string& field_name, int a)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
 
   if (find_create_key_field(key, field_name, myvec) != true) {
     return 0;
   }
+  auto sm = (*myvec)[cur_window];
+
+  sm.second->add(a);
+  update_write_timestamp((*myvec)[cur_window]);
+  return sm.second->sum(*myvec);
 }
 
+template <typename T>
+int TWStatsDB<T>::add(const T& key, const std::string& field_name, const std::string& s)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
+
+  if (find_create_key_field(key, field_name, myvec) != true) {
+    return 0;
+  }
+  auto sm = (*myvec)[cur_window];
+
+  sm.second->add(s);
+  update_write_timestamp((*myvec)[cur_window]);
+  return sm.second->sum(*myvec);
+}
+
+template <typename T>
+int TWStatsDB<T>::sub(const T& key, const std::string& field_name, int a)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
+
+  if (find_create_key_field(key, field_name, myvec) != true) {
+    return 0;
+  }
+  auto sm = (*myvec)[cur_window];
+
+  sm.second->sub(a);
+  update_write_timestamp((*myvec)[cur_window]);
+  return sm.second->sum(*myvec);
+}
+
+template <typename T>
+int TWStatsDB<T>::sub(const T& key, const std::string& field_name, const std::string& s)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
+
+  if (find_create_key_field(key, field_name, myvec) != true) {
+    return 0;
+  }
+  auto sm = (*myvec)[cur_window];
+
+  sm.second->sub(s);
+  update_write_timestamp((*myvec)[cur_window]);
+  return sm.second->sum(*myvec);
+}
+
+template <typename T>
+int TWStatsDB<T>::get(const T& key, const std::string& field_name)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
+
+  if (find_key_field(key, field_name, myvec) != true) {
+    return 0;
+  }
+  auto sm = (*myvec)[cur_window];
+
+  return sm.second->sum(*myvec);
+}
+
+template <typename T>
+int TWStatsDB<T>::get_current(const T& key, const std::string& field_name)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
+
+  if (find_key_field(key, field_name, myvec) != true) {
+    return 0;
+  }
+  auto sm = (*myvec)[cur_window];
+
+  return sm.second->get();
+}
+
+template <typename T>
+bool TWStatsDB<T>::get_windows(const T& key, const std::string& field_name, std::vector<int>& ret_vec)
+{
+  TWStatsBuf* myvec;
+  std::lock_guard<std::mutex> lock(mutx);
+
+  if (find_key_field(key, field_name, myvec) != true) {
+    return false;
+  }
+  for (auto sm : (*myvec)) {
+    ret_vec.push_back(sm.second->get());
+  }
+
+  return(true);
+}
+
+#include <string.h>
+#include "hyperloglog.hpp"
 
 main (int argc, char** argv)
 {
-  auto ap=createInstance<TWStatsMemberInt>();
-  auto bp=createInstance<TWStatsMemberInt>();
-  std::vector<TWStatsMemberP> vec;
-
-  ap->add(10);
-  bp->add(10);
-  vec.push_back(ap);
-  vec.push_back(bp);
-
-  // std::cout << ap->sum(vec) << "\n";
-
-  TWStatsDB<std::string> sdb(600,6);
+  TWStatsDB<std::string> sdb(1,60);
   auto field_map = std::make_shared<FieldMap>();
+
   (*field_map)[std::string("numWidgets")] = std::string("int");
+  (*field_map)[std::string("diffWidgets")] = std::string("hll");
   sdb.setFields(field_map);
-  sdb.incr("neil.cook", "numWidgets");
+
+  // test integers
+  for (int i=0; i<10; i++) {
+    sdb.incr("neil.cook", "numWidgets");
+    sdb.incr("neil.cook", "numWidgets");
+    int k = sdb.decr("neil.cook", "numWidgets");
+    sleep(1);
+    // std::cout << k << "\n";
+  }
+  std::cout << "get_current is " << sdb.get_current("neil.cook", "numWidgets") << "\n";
+  std::cout << "get is " << sdb.get("neil.cook", "numWidgets") << "\n";
+  std::vector<int> vec;
+  sdb.get_windows("neil.cook", "numWidgets", vec);
+  for (auto i : vec) {
+    std::cout << "window has value " << i << "\n";
+  }  
+
+  // test hyperloglog
+  for (int i=0; i<10; i++) {
+    sdb.add("neil.cook", "diffWidgets", "widget1");
+    sdb.add("neil.cook", "diffWidgets", "widget2");
+    sdb.add("neil.cook", "diffWidgets", "widget3");
+    sleep(1);
+    // std::cout << k << "\n";
+  }
+  std::cout << "get_current is " << sdb.get_current("neil.cook", "diffWidgets") << "\n";
+  std::cout << "get is " << sdb.get("neil.cook", "diffWidgets") << "\n";
+  vec.clear();
+  sdb.get_windows("neil.cook", "diffWidgets", vec);
+  for (auto i : vec) {
+    std::cout << "window has value " << i << "\n";
+  }  
+  
 }
--- twmap.hh
+++ twmap.hh
@@ -2,26 +2,30 @@
 #include <vector>
 #include <memory>
 #include <map>
+#include <unordered_map>
 #include <ctime>
+#include <mutex>
+#include <thread>
+#include "hyperloglog.hpp"
 
 class TWStatsMember;
 
 typedef std::shared_ptr<TWStatsMember> TWStatsMemberP;
+typedef std::vector<std::pair<std::time_t, TWStatsMemberP>> TWStatsBuf;
 
 // Not all stats member subclasses will implement all of these methods in a useful way (e.g. incr is meaningless to a hll)
 class TWStatsMember
 {
 public:
-  virtual int incr() = 0; // add 1 to the stored value
-  virtual int decr() = 0; // subtract 1 from the stored value
-  virtual int add(int a) = 0; // add an integer to the stored value
-  virtual int add(const std::string& s) = 0; // add a string to the stored window (this has different semantics for each subclass)
-  virtual int sub(int a) = 0; // subtract an integer from the stored value
-  virtual int sub(const std::string& s) = 0; // remove a string from the stored value
+  virtual void add(int a) = 0; // add an integer to the stored value
+  virtual void add(const std::string& s) = 0; // add a string to the stored window (this has different semantics for each subclass)
+  virtual void sub(int a) = 0; // subtract an integer from the stored value
+  virtual void sub(const std::string& s) = 0; // remove a string from the stored value
   virtual int get() = 0; // return the current stat
   virtual void set(int) = 0; // Set the stored value to the supplied integer
   virtual void set(const std::string& s) = 0; // Set the stored value based on the supplied string
-  virtual int sum(const std::vector<TWStatsMemberP>& vec) = 0; // combine an array of stored values
+  virtual void erase() = 0; // Remove all data - notice this isn't necessarily the same as set(0) or set("")
+  virtual int sum(const TWStatsBuf& vec) = 0; // combine an array of stored values
 };
 
 class TWStatsMemberInt;
@@ -31,21 +35,20 @@ typedef std::shared_ptr<TWStatsMemberInt> TWStatsMemberIntP;
 class TWStatsMemberInt : public TWStatsMember
 {
 public:
-  int incr() { i++; return i; }
-  int decr() { i--; return i; }
-  int add(int a) { i += a; return i; }
-  int add(const std::string& s) { return i; }
-  int sub(int a) { i -= a; return i; }
-  int sub(const std::string& s) { return i; }
+  void add(int a) { i += a; }
+  void add(const std::string& s) { return; }
+  void sub(int a) { i -= a;  }
+  void sub(const std::string& s) { return; }
   int get() { return i; }
   void set(int a) { i = a; }
   void set(const std::string& s) { return; }
-  int sum(const std::vector<TWStatsMemberP>& vec)
+  void erase() { i = 0; }
+  int sum(const TWStatsBuf& vec)
   {
     int count = 0;
     for (auto a : vec)
       {
-        count += a->get();
+        count += a.second->get();
       }
     return count;
   }
@@ -53,17 +56,52 @@ private:
   int i=0;
 };
 
+#define HLL_NUM_REGISTER_BITS 10
+
+class TWStatsMemberHLL : public TWStatsMember
+{
+public:
+  TWStatsMemberHLL()
+  {
+    hllp = std::make_shared<hll::HyperLogLog>(HLL_NUM_REGISTER_BITS);
+  }
+  void add(int a) { char buf[64]; int len = snprintf(buf, 63, "%d", a); hllp->add(buf, len); return; }
+  void add(const std::string& s) { hllp->add(s.c_str(), s.length()); return; }
+  void sub(int a) { return; }
+  void sub(const std::string& s) { return; }
+  int get() { return hllp->estimate(); }
+  void set(int a) { return; }
+  void set(const std::string& s) { hllp->clear(); hllp->add(s.c_str(), s.length()); }
+  void erase() { hllp->clear(); }
+  int sum(const TWStatsBuf& vec)
+  {
+    int count = 0;
+    hll::HyperLogLog hllsum(HLL_NUM_REGISTER_BITS);
+    for (auto a : vec)
+      {
+	// XXX yes not massively pretty
+	std::shared_ptr<TWStatsMemberHLL> twp = std::dynamic_pointer_cast<TWStatsMemberHLL>(a.second);
+	hllsum.merge((*(twp->hllp)));
+      }
+    return hllsum.estimate();
+  }
+private:
+  std::shared_ptr<hll::HyperLogLog> hllp;
+};
+
+
 template<typename T> TWStatsMemberP createInstance() { return std::make_shared<T>(); }
 
 typedef std::map<std::string, TWStatsMemberP(*)()> map_type;
 
-// This needs to be instanstiated
+// This is instantiated in twmap.cc
 struct TWStatsTypeMap
 {
   map_type type_map;
   TWStatsTypeMap()
   {
     type_map["int"] = &createInstance<TWStatsMemberInt>;
+    type_map["hll"] = &createInstance<TWStatsMemberHLL>;
   }
 };
 
@@ -76,30 +114,49 @@ class TWStatsDB
 public:
   explicit TWStatsDB(int w_siz, int num_w)
   {
-    window_size = w_siz;
-    num_windows = num_w;
+    window_size = w_siz ? w_siz : 1;
+    num_windows = num_w ? num_w : 1;
     std::time(&start_time);
   }
   bool setFields(std::shared_ptr<FieldMap> fieldsp);
   int incr(const T& key, const std::string& field_name);
   int decr(const T& key, const std::string& field_name);
-  int add(const T& key, const std::string& field_name, int a);
-  int add(const T& key, const std::string& field_name, const std::string& s);
-  int sub(const T& key, const std::string& field_name, int a);
-  int sub(const T& key, const std::string& field_name, const std::string& s);
+  int add(const T& key, const std::string& field_name, int a); // returns all fields summed/combined
+  int add(const T& key, const std::string& field_name, const std::string& s); // returns all fields summed/combined
+  int sub(const T& key, const std::string& field_name, int a); // returns all fields summed/combined
+  int sub(const T& key, const std::string& field_name, const std::string& s); // returns all fields summed/combined
   int get(const T& key, const std::string& field_name); // gets all fields summed/combined over all windows
   int get_current(const T& key, const std::string& field_name); // gets the value just for the current window
-  std::vector<int> get_windows(const T& key, const std::string& field_name); // gets each window value returned in a vector
+  bool get_windows(const T& key, const std::string& field_name, std::vector<int>& ret_vec); // gets each window value returned in a vector
   void set(const T& key, const std::string& field_name, int a);
   void set(const T& key, const std::string& field_name, const std::string& s);
 protected:
-  bool find_create_key_field(const T& key, const std::string& field_name, std::vector<TWStatsMemberP>& ret_vecfg
-);
+  bool find_create_key_field(const T& key, const std::string& field_name, TWStatsBuf*& ret_vec, bool create=1);
+  bool find_key_field(const T& key, const std::string& field_name, TWStatsBuf*& ret_vec);  
+  int current_window() {
+    std::time_t now, diff;
+    std::time(&now);
+    diff = now - start_time;
+    int cw = (diff/window_size) % num_windows;
+  }
+  void update_write_timestamp(std::pair<std::time_t, TWStatsMemberP>& vec)
+  {
+    // only do this if the window first mod time is 0
+    if (vec.first == 0) {
+      std::time_t now, write_time;
+      std::time(&now);
+      write_time = now - ((now - start_time) % window_size);
+      vec.first = write_time;
+    }
+  }
+  void clean_windows(TWStatsBuf& twsbuf);
 private:
-  typedef std::map<T, std::map<std::string, std::vector<TWStatsMemberP>>> TWStatsDBMap;
+  typedef std::unordered_map<T, std::map<std::string, TWStatsBuf>> TWStatsDBMap;
   TWStatsDBMap stats_db;
   std::shared_ptr<FieldMap> field_map;
   int window_size;
   int num_windows;
+  int cur_window = 0;
   std::time_t start_time;
+  mutable std::mutex mutx;
 };

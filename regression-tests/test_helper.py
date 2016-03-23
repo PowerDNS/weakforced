2cec55cab5db826eb61efee8dae5c9c5272db933
Neil Cook <neil.cook@noware.co.uk>
2016-03-23 19:59:47 +0100

    unbound library for travis

--- regression-tests/test_helper.py
+++ regression-tests/test_helper.py
@@ -19,7 +19,7 @@ class ApiTestCase(unittest.TestCase):
         self.session = requests.Session()
         self.session.auth = ('foo', os.environ.get('APIKEY', 'super'))
         self.session.keep_alive = False
-        self.session.headers = {"Connection": "close"} # FIXME this obscures a bug in wforce http
+#        self.session.headers = {"Connection": "close"} # FIXME this obscures a bug in wforce http
 #        self.session.headers = {'X-API-Key': os.environ.get('APIKEY', 'changeme-key'), 'Origin': 'http://%s:%s' % (self.server_address, self.server_port)}
 
     def writeFileToConsole(self, file):

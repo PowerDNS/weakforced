8b0440093967d36f26a96c31cc0f2ae44d073b45
Neil Cook <neil.cook@noware.co.uk>
2016-07-18 14:21:13 +0100

    Fix 'boolean success' regression, docs and allow -c without argument

--- regression-tests/test_TimeWindows.py
+++ regression-tests/test_TimeWindows.py
@@ -16,7 +16,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(25):
-            r = self.reportFunc('ivbaddie', '127.0.0.1', "1234'%s" % i, 'true')
+            r = self.reportFunc('ivbaddie', '127.0.0.1', "1234'%s" % i, True)
             r.json()
 
         r = self.allowFunc('ivbaddie', '127.0.0.1', "1234")
@@ -37,7 +37,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(31):
-            r = self.reportFunc('flbaddie', '128.0.0.1', "1234", 'false')
+            r = self.reportFunc('flbaddie', '128.0.0.1', "1234", False)
             r.json()
 
         r = self.allowFunc('flbaddie', '128.0.0.1', "1234")
@@ -58,7 +58,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(12):
-            r = self.reportFunc('ipbaddie', "127.0.0.%s" % i, "1234", 'true')
+            r = self.reportFunc('ipbaddie', "127.0.0.%s" % i, "1234", True)
             r.json()
 
         r = self.allowFunc('ipbaddie', '127.0.0.1', "1234")
@@ -79,7 +79,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(41):
-            r = self.reportFunc('subbaddie', '128.0.0.1', "1234", 'false')
+            r = self.reportFunc('subbaddie', '128.0.0.1', "1234", False)
             r.json()
 
         r = self.allowFunc('subbaddie', '128.0.0.1', "1234")
@@ -100,7 +100,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(12):
-            r = self.reportFunc('ipv4baddie%s' % i, "114.31.192.%s" % i, "1234", 'true')
+            r = self.reportFunc('ipv4baddie%s' % i, "114.31.192.%s" % i, "1234", True)
             r.json()
 
         r = self.allowFunc('ipv4baddie', '114.31.192.200', "1234")
@@ -121,7 +121,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(12):
-            r = self.reportFunc('ipv6baddie%s' % i, "2001:c78::%s" % i, "1234", 'true')
+            r = self.reportFunc('ipv6baddie%s' % i, "2001:c78::%s" % i, "1234", True)
             r.json()
 
         r = self.allowFunc('ipv6baddie', '2001:c78::1000', "1234")
@@ -142,7 +142,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(40):
-            r = self.reportFunc('expirebaddie%s' % i, "127.0.0.1", "1234", 'true')
+            r = self.reportFunc('expirebaddie%s' % i, "127.0.0.1", "1234", True)
             r.json()
 
         r = self.allowFunc('expirebaddie', '127.0.0.1', "1234")
@@ -163,7 +163,7 @@ class TestTimeWindows(ApiTestCase):
         r.close()
 
         for i in range(31):
-            r = self.reportFunc('resetbaddie', '128.0.0.1', "1234", 'false')
+            r = self.reportFunc('resetbaddie', '128.0.0.1', "1234", False)
             r.json()
 
         r = self.allowFunc('resetbaddie', '128.0.0.1', "1234")

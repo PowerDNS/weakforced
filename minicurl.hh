f8fd978ac326169d6196d1f87f8916811f0fc431
Neil Cook <neil.cook@noware.co.uk>
2016-08-22 17:23:45 +0100

    Minicurl POST and general improvements

--- minicurl.hh
+++ minicurl.hh
@@ -1,7 +1,32 @@
-pragma once
+/*
+ * This file is part of PowerDNS or weakforced.
+ * Copyright -- PowerDNS.COM B.V. and its contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * In addition, for the avoidance of any doubt, permission is granted to
+ * link this program with OpenSSL and to (re)distribute the binaries
+ * produced as the result of such linking.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+#include <map>
 #include <string>
 #include <curl/curl.h> 
 // turns out 'CURL' is currently typedef for void which means we can't easily forward declare it
+
+using MiniCurlHeaders = std::map<std::string, std::string>;
 
 class MiniCurl
 {
@@ -9,9 +34,17 @@ public:
   MiniCurl();
   ~MiniCurl();
   MiniCurl& operator=(const MiniCurl&) = delete;
-  std::string getURL(const std::string& str);
+  std::string getURL(const std::string& url, const MiniCurlHeaders& headers);
+  void setCurlOption(CURLoption option, ...);
+  bool postURL(const std::string& url, const std::string& post_body, const MiniCurlHeaders& headers,
+	       std::string& error_msg);
+protected:
+  void setCurlHeaders(const MiniCurlHeaders& headers, struct curl_slist** header_listp);
+  void clearCurlHeaders(struct curl_slist* header_list);
 private:
   CURL *d_curl;
   static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
+  static size_t read_callback(char *buffer, size_t size, size_t nitems, void *userdata);
   std::string d_data;
+  char d_error_buf[CURL_ERROR_SIZE];
 };
--- webhook.hh
+++ webhook.hh
@@ -1,9 +1,32 @@
+/*
+ * This file is part of PowerDNS or weakforced.
+ * Copyright -- PowerDNS.COM B.V. and its contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * In addition, for the avoidance of any doubt, permission is granted to
+ * link this program with OpenSSL and to (re)distribute the binaries
+ * produced as the result of such linking.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #pragma once
 #include <mutex>
 #include <curl/curl.h>
 #include "wforce.hh"
 #include "ext/json11/json11.hpp"
 #include "ext/ctpl.h"
+#include "dolog.hh"
 
 using namespace json11;
 
@@ -272,105 +295,18 @@ typedef std::map<unsigned int, std::vector<CurlConnection>> CurlConnMap;
 class WebHookRunner
 {
 public:
-  WebHookRunner(unsigned int num_threads):p(num_threads, 1000)
-  {
-    curl_global_init(CURL_GLOBAL_ALL);
-  }
-  ~WebHookRunner()
-  {
-    for (auto& ci : conns) {
-      for (auto& i : ci.second) {
-	curl_easy_cleanup(i.curl);
-      }
-    }
-  }
-  void setNumThreads(unsigned int num_threads)
-  {
-    p.resize(num_threads);
-  }
-  void setMaxConns(unsigned int max_conns)
-  {
-    max_hook_conns = max_conns;
-  }
+  WebHookRunner(unsigned int num_threads);
+  ~WebHookRunner();
+  void setNumThreads(unsigned int num_threads);
+  void setMaxConns(unsigned int max_conns);
   // synchronously run the ping command for the hook
-  bool pingHook(const WebHook& hook, std::string error_msg)
-  {
-    std::shared_ptr<std::mutex> conn_mutex;
-    CURL* curl;
-    getConnection(hook.getID(), &curl, conn_mutex); // this will only return once it has a connection
-
-    // do HTTP stuff here
-    
-    releaseConnection(conn_mutex);
-    return true;
-  }
+  bool pingHook(const WebHook& hook, std::string error_msg);
   // asynchronously run the hook with the supplied data (must be in json format)
-  void runHook(const std::string& event_name, const WebHook& hook, const Json& hook_json)
-  {
-    std::shared_ptr<std::mutex> conn_mutex;
-    CURL* curl;
-    getConnection(hook.getID(), &curl, conn_mutex); // this will only return once it has a connection
-    // the called function is responsible for releasing the mutex
-    p.push(_runHook, event_name, hook, hook_json, conn_mutex); 
-  }
+  void runHook(const std::string& event_name, const WebHook& hook, const Json& hook_json);
 protected:
-  void getConnection(unsigned int hook_id, CURL** curlp, std::shared_ptr<std::mutex>& out_mutex)
-  {
-    std::lock_guard<std::mutex> lock(conn_mutex);
-    
-    auto ci = conns.find(hook_id);
-    if (ci != conns.end()) { // we already have some connections, maybe we can reuse
-      // first we try to get an existing connection
-      for (auto& i : ci->second) {
-	if (i.cmutex->try_lock()) {
-	  *curlp = i.curl;
-	  out_mutex = i.cmutex;
-	  return;
-	}
-	else
-	  continue;
-      }
-      // ok, nothing doing so we create a new connection if we can
-      unsigned int num_conns = ci->second.size();
-      if (num_conns < max_hook_conns) {
-	CurlConnection cc = CurlConnection();
-	cc.cmutex->lock();
-	*curlp = cc.curl;
-	out_mutex = cc.cmutex;
-	ci->second.push_back(cc);
-      }
-      else {
-	// ok so we couldn't create a new connection so let's just wait for an
-	// existing connection to become available
-	// XXX if very busy, we'll have a bunch of threads trying to get the first lock
-	for (auto& i : ci->second) {
-	  i.cmutex->lock();
-	  *curlp = i.curl;
-	  out_mutex = i.cmutex;
-	  return;
-	}
-      }
-    }
-    else { // first time for this hook id
-      std::vector<CurlConnection> vec;
-      CurlConnection cc = CurlConnection();
-      cc.cmutex->lock();
-      *curlp = cc.curl;
-      out_mutex = cc.cmutex;
-      vec.push_back(cc);
-      conns.insert(std::make_pair(hook_id, vec));
-    }
-  }
-  static void releaseConnection(std::shared_ptr<std::mutex> conn_mutex)
-  {
-    conn_mutex->unlock();
-  }
-  static void _runHook(int id, const std::string& event_name, const WebHook& hook, const Json& hook_json, std::shared_ptr<std::mutex> conn_mutex)
-  {
-    noticelog("Webhook id=%d starting for event (%s) to url (%s)",
-	      hook.getID(), event_name, hook.getConfigKey("url"));
-    releaseConnection(conn_mutex);
-  }
+  void getConnection(unsigned int hook_id, CURL** curlp, std::shared_ptr<std::mutex>& out_mutex);
+  static void releaseConnection(std::shared_ptr<std::mutex> conn_mutex);
+  static void _runHook(int id, const std::string& event_name, const WebHook& hook, const Json& hook_json, std::shared_ptr<std::mutex> conn_mutex);
 private:
   ctpl::thread_pool p;
   unsigned int max_hook_conns = MAX_HOOK_CONN;

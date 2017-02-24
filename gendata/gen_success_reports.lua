73af9ca49202b41f3cb521f264b404935073d2bb
Neil Cook <neil.cook@noware.co.uk>
2017-02-24 11:37:23 +0000

    Utility scripts to generate data for wforce
    These are not load generation scripts, rather they are intended
    to be used to populate logstash/elasticsearch with report data
    using the named report sink feature.

---
+++ gendata/gen_success_reports.lua
@@ -0,0 +1,56 @@
+# This lua script generates random reports for wforce logins
+
+counter = 0
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["Authorization"] = "Basic d2ZvcmNlOnN1cGVy"
+wrk.method = 'POST'
+wrk.path = "/?command=report"
+
+dp_access = {
+   {protocol="http", device="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"},
+   {protocol="https", device="Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25"},
+   {protocol="https", device="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"},
+   {protocol="http", device="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36"},
+   {protocol="https", device="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"},
+   {protocol="http", device="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 7.0; InfoPath.3; .NET CLR 3.1.40767; Trident/6.0; en-IN)"},
+   {protocol="https", device="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"},
+   {protocol="imap", device='\\"name\\" \\"Mac OS X Mail\\" \\"version\\" \\"10.0 (3226)\\" \\"os\\" \\"Mac OS X\\" \\"os-version\\" \\"10.12 (16A323)\\" \\"vendor\\" \\"Apple Inc.\\"'}
+}
+
+ip_access = {
+   "23.96.52.53",
+   "86.128.243.146",
+   "213.20.82.16",
+   "212.223.44.11",
+   "8.8.8.8",
+   "2a03:b0c0:2:d0::4ab:8001",
+   "2a00:1450:4009:80a::200e",
+   "2a00:1450:4009:806::2005",
+   "180.22.47.32",
+   "52.48.64.3"
+}
+
+pwd_access = {
+   "ijfds",
+   "sokee",
+   "sdefw",
+   "93ked",
+   "0kwepw",
+   "ow3002",
+   "2303wp",
+   "2020ew",
+   "309oka"
+}
+
+request = function()
+   ip_mod = counter % table.getn(ip_access) + 1
+   dp_mod = counter % table.getn(dp_access) + 1
+   pwd_mod = counter % table.getn(pwd_access) + 1
+   mybody = '{"login":"user' .. counter .. '", "remote":"' .. ip_access[ip_mod] .. '", "pwhash":"' .. pwd_access[pwd_mod] .. '", "success": true, "policy_reject":false, "protocol":"' .. dp_access[dp_mod].protocol .. '", "device_id":"' .. dp_access[dp_mod].device .. '" }'
+   counter = counter + 1
+   if (counter==1000)
+        then
+                counter = 0
+        end
+   return wrk.format(nil, nil, nil, mybody)
+end
---
+++ gendata/send_reports.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Shell-script style.
+
+import os
+import requests
+import shutil
+import subprocess
+import sys
+import tempfile
+import time
+
+WEBPORT = '8084'
+APIKEY = 'super'
+
+cmd1 = ("../wforce -C ./wforce_elastic.conf -R ../regexes.yaml").split()
+wrksuccesscmd = ("wrk -c 10 -d 60 -t 1 -s ./gen_success_reports.lua -R 50 http://127.0.0.1:8084").split()
+wrkfailcmd = ("wrk -c 10 -d 30 -t 1 -s ./gen_fail_reports.lua -R 50 http://127.0.0.1:8084").split()
+
+# Now run wforce and the tests.
+print "Launching wforce..."
+print ' '.join(cmd1)
+proc1 = subprocess.Popen(cmd1, close_fds=True)
+wforcepid = proc1.pid
+
+print "Waiting for webserver port to become available..."
+available = False
+for try_number in range(0, 10):
+    try:
+        res = requests.get('http://127.0.0.1:%s/' % WEBPORT)
+        available = True
+        break
+    except:
+        time.sleep(0.5)
+
+if not available:
+    print "Webserver port not reachable after 10 tries, giving up."
+    proc1.terminate()
+    proc1.wait()
+    sys.exit(2)
+
+print "Sending Reports..."
+wrkproc = subprocess.Popen(wrksuccesscmd, close_fds=True)
+wrkproc.wait()
+wrkproc = subprocess.Popen(wrkfailcmd, close_fds=True)
+wrkproc.wait()
+print "Done sending reports..."
+proc1.terminate()
+proc1.wait()
+print "Exiting"
+sys.exit(0)

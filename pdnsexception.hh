7022be1f5c01d84a5a2d9c185aed6e4f559f689d
Neil Cook <neil.cook@noware.co.uk>
2017-04-14 07:34:30 +0200

    Add pdnsexception.hh from pdns repo

---
+++ pdnsexception.hh
@@ -0,0 +1,47 @@
+/*
+ * This file is part of PowerDNS or dnsdist.
+ * Copyright -- PowerDNS.COM B.V. and its contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * In addition, for the avoidance of any doubt, permission is granted to
+ * link this program with OpenSSL and to (re)distribute the binaries
+ * produced as the result of such linking.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#ifndef PDNSEXCEPTION_HH
+#define PDNSEXCEPTION_HH
+/* (C) 2002 POWERDNS.COM BV */
+
+#include<string>
+
+#include "namespaces.hh"
+
+//! Generic Exception thrown 
+class PDNSException
+{
+public:
+  PDNSException(){reason="Unspecified";};
+  PDNSException(string r){reason=r;};
+  
+  string reason; //! Print this to tell the user what went wrong
+};
+
+class TimeoutException : public PDNSException
+{
+public:
+  TimeoutException() : PDNSException() {}
+  TimeoutException(string r) : PDNSException(r) {}
+};
+
+#endif

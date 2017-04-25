bcc628f9a3a40bedc579c2ec1a78516fa261044d
Neil Cook <neil.cook@noware.co.uk>
2017-04-25 15:41:31 +0100

    Replace useless qa-latency with perfstats in stats API command

--- wforce-web.cc
+++ wforce-web.cc
@@ -542,7 +542,7 @@ void parseStatsCmd(const YaHTTP::Request& req, YaHTTP::Response& resp, const std
     { "user-msec", (int)(ru.ru_utime.tv_sec*1000ULL + ru.ru_utime.tv_usec/1000) },
     { "sys-msec", (int)(ru.ru_stime.tv_sec*1000ULL + ru.ru_stime.tv_usec/1000) },
     { "uptime", uptimeOfProcess()},
-    { "qa-latency", (int)g_stats.latency}
+    { "perfstats", perfStatsToJson()}
   };
 
   resp.status=200;

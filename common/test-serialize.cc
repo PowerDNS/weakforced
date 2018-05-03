4437581ef18a8e5869b16abf7fae70561ec19852
Neil Cook <neil.cook@noware.co.uk>
2018-05-03 10:40:35 +0100

    Unit tests for StatsB dump and restore

--- common/test-serialize.cc
+++ common/test-serialize.cc
@@ -3,15 +3,13 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include "twmap.hh"
 #include <boost/test/unit_test.hpp>
 
 #include "ext/hyperloglog.hpp"
 #include "ext/count_min_sketch.hpp"
 #include "misc.hh"
-
 #include <iostream>
-
-using namespace std;
 
 BOOST_AUTO_TEST_SUITE(test_serialize)
 
@@ -56,6 +54,48 @@ BOOST_AUTO_TEST_CASE(test_serialize_countmin) {
   cmcopy2.restore(ss2);
 
   BOOST_CHECK((int)cmcopy2.estimate(foo.c_str()) == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_serialize_statsdb) {
+  TWStatsDB<std::string> sdb1("sdb1", 600, 6);
+  TWStatsDB<std::string> sdb2("sdb2", 600, 6);
+  FieldMap fm;
+
+  fm.insert(std::make_pair("field1", "int"));
+  fm.insert(std::make_pair("field2", "hll"));
+  fm.insert(std::make_pair("field3", "countmin"));
+  sdb1.setFields(fm);
+  sdb2.setFields(fm);
+  
+  sdb1.add(std::string("bar"), "field1", 10);
+  sdb1.add(std::string("bar"), "field2", "diff1");
+  sdb1.add(std::string("bar"), "field3", "diff1");
+
+  sdb1.add(std::string("foo"), "field1", 10);
+  BOOST_CHECK(sdb1.get(std::string("foo"), "field1") == 10);
+
+  sdb1.add(std::string("foo"), "field2", "diff1");
+  sdb1.add(std::string("foo"), "field2", "diff2");
+  sdb1.add(std::string("foo"), "field2", "diff3");
+  BOOST_CHECK(sdb1.get(std::string("foo"), "field2") == 3);
+  
+  sdb1.add(std::string("foo"), "field3", "diff1");
+  sdb1.add(std::string("foo"), "field3", "diff1");
+  sdb1.add(std::string("foo"), "field3", "diff2");
+  BOOST_CHECK(sdb1.get(std::string("foo"), "field3", "diff1") == 2);
+
+  for (auto it = sdb1.startDBDump(); it != sdb1.DBDumpIteratorEnd(); ++it) {
+    TWStatsDBDumpEntry e;
+    std::string key;
+    if (sdb1.DBDumpEntry(it, e, key)) {
+      sdb2.restoreEntry(key, e);
+    }
+  }
+  sdb1.endDBDump();
+
+  BOOST_CHECK(sdb2.get(std::string("foo"), "field1") == 10);
+  BOOST_CHECK(sdb2.get(std::string("foo"), "field2") == 3);
+  BOOST_CHECK(sdb2.get(std::string("foo"), "field3", "diff1") == 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END();

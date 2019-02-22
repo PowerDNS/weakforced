68e51f85971a35815dc51beebf9d764b3f1d27c6
Neil Cook <neil.cook@noware.co.uk>
2019-02-22 09:23:48 +0000

    Handle python3 behaviour regarding file object flushing and bytes vs strings

--- regression-tests/udp_sink.py
+++ regression-tests/udp_sink.py
@@ -8,12 +8,14 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
-logfile = open('/tmp/udp-sink.log', 'w', 0)
+logfile = open('/tmp/udp-sink.log', 'w')
 logfile.write("This is to ensure the file isn't empty\n")
+logfile.flush()
 mypid = os.getpid()
 
 while True:
     data, addr = sock.recvfrom(2048) # buffer size is 2048 bytes
     log_msg = "[%s] Received report=%s\n" % (mypid, data)
     logfile.write(log_msg)
+    logfile.flush()
     print(log_msg)

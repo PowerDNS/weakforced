3ee9e3e2a301d4c05d87b478f72682563b599152
Neil Cook <neil.cook@noware.co.uk>
2020-04-27 16:55:43 +0100

    Docker image configuration via environment variables
    - Provide limited configurability for built-in config using Jinja2 templating
    - Allow override config file to be specified
    - Don't allow the docker container to be started without specifying passwords
    - Tag the image as powerdns/wforce

--- docker/image/docker-entrypoint.sh
+++ docker/image/docker-entrypoint.sh
@@ -12,6 +12,27 @@ else
   exec "$@"
 fi
 
-exec /usr/bin/wforce -s
+WFORCE_CMD="/usr/bin/wforce -D"
+
+if [[ "$WFORCE_VERBOSE" -eq "1" ]]; then
+    WFORCE_CMD="$WFORCE_CMD -v"
+fi
+
+if [[ "x$WFORCE_CONFIG_FILE" == "x" ]]; then
+    if [[ "x$WFORCE_HTTP_PASSWORD" == "x" ]]; then
+        2>&1 echo "WFORCE_HTTP_PASSWORD environment variable must be set"
+    else
+        echo "Note you are using the default config file - to take full advantage of the capabilities of weakforced, you should specify a custom config file with the WFORCE_CONFIG_FILE environment variable."
+        create_config.sh /etc/wforce/wforce.conf.j2 /etc/wforce/wforce.conf
+        echo "Starting $WFORCE_CMD"
+        exec $WFORCE_CMD
+    fi
+else
+    WFORCE_CMD="$WFORCE_CMD -C $WFORCE_CONFIG_FILE"
+    exec $WFORCE_CMD
+fi
+
+exit 1
+
 
 

064a1bc369c09555123c7767585024fad984be36
Neil Cook <neil.cook@noware.co.uk>
2020-08-06 16:06:38 +0100

    Push built docker image to docker hub, and only when on master branch

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 TAG=`git describe --tags`
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag powerdns/wforce:$TAG neilcookster/wforce:$TAG
-docker push neilcookster/wforce:$TAG
-# Enable this at some point
-# docker push powerdns/wforce:$TAG
+docker push powerdns/wforce:$TAG

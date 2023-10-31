2f4b4b60d6a874dfdbd9b91d8341b2fe2639a756
Neil Cook <neil.cook@noware.co.uk>
2023-10-31 14:57:18 +0000

    Remove report api from weakforced

--- report_api/runreport.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import sys
-sys.path.append(sys.path[0])
-from wforce import app
-if __name__ == "__main__":
-	app.run(debug=True, use_reloader=False)
--- report_api/wforce/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, instance_relative_config=True)
-from wforce import report
